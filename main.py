18f2e6d7247c25e58bb8f03c67c62b761879b201
Mahesh <maheshh.kumar1508@gmail.com>
2026-09-18 20:36:15 +0530

    path parameters

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ app = FastAPI();
 @app.get("/")
 def home():
     return {"msg":"helloowwwwwww"}
+
+# path parameters
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    return {"user_id":user_id}
+
+#query parameters
+@app.get("/users")
+def get_user(name:str ="rohit"):
+    return {"name":name}
+# multiple query parameters
+@app.get("/item")
+def get_user(name:str ="Brush", price:int=90):
+    return {"name":name, "price":price}
