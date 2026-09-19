240374e2f86cf4ee501334aa503738884d187b62
Mahesh <maheshh.kumar1508@gmail.com>
2026-09-19 11:32:46 +0530

    post req

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI();
 
@@ -19,3 +20,32 @@ def get_user(name:str ="rohit"):
 @app.get("/item")
 def get_user(name:str ="Brush", price:int=90):
     return {"name":name, "price":price}
+
+# post request
+
+@app.post("/create-user")
+def create_user(name:str, age:int):
+    return {
+        "success":"true",
+        "message":f'hello {name}, your age is {age}'
+    }
+
+@app.post("/create-user-dict")
+def create_user_dict(details:dict):
+    return {
+        "success":"true",
+        "data":details
+    }
+
+# use pydanic for data validation
+class User(BaseModel):
+    name:str
+    age:int
+    address:str
+@app.post("/create-user-pydantic")
+def create_user_dict(details:User):
+    return {
+        "success":"true",
+        "data":details
+    }
+
