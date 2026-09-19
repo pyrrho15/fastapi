32d15a2d7e54f8383326e9e9b44e294f00bfd074
Mahesh <maheshh.kumar1508@gmail.com>
2026-09-19 23:57:11 +0530

    simple todo api

--- main.py
+++ main.py
@@ -63,3 +63,45 @@ def create_user_dict(details:User):
         "success":"true",
         "data":details
     }
+
+
+# simple todo app
+todos = []
+class Todo(BaseModel):
+    id:int
+    title:str
+    isCompleted:bool
+
+@app.post("/create_todo")
+def create_todo(todo:Todo):
+    todos.append(todo)
+    return {"message":"todo created"}
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.get("/todo/{id}")
+def get_todos(id:int):
+    for todo in todos:
+        if todo.id == id:
+            return todo
+    return {"error":"todo not found"}
+
+@app.put("/todo/{id}")
+def update_todo(id:int, updated_todo:Todo):
+    for index,todo in enumerate(todos):
+        if todo.id == id:
+            todos[index] = updated_todo
+            return {"msg":"updated", "data":updated_todo}
+        else:
+            return {"error":"todo not found"}
+
+@app.delete("/todo/{id}")
+def delete_todo(id:int):
+    for index,todo in enumerate(todos):
+        if todo.id == id:
+            todos.pop(index)
+            return {"msg":"deleted"}
+        else:
+            return {"error":"todo not found"}
