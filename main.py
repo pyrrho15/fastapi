764caf2fe076e02e4b94812bcb0d28c78658572b
Mahesh <maheshh.kumar1508@gmail.com>
2026-09-23 20:37:40 +0530

    fastapi + db

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from sqlalchemy import column, create_engne, Column, Integer, String, true
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import column, create_engine, Column, Integer, String, true
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI, Depends
 
+app=FastAPI()
 DB_URL="sqlite:///./test.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread":False})
@@ -16,3 +18,16 @@ class Todo(Base):
     completed=Column(String)
 
 Base.metadata.create_all(bind=engine) 
+
+def get_db():
+    db=sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+@app.get("/")
+def home(db:Session=Depends(get_db)):
+    return{
+        "message":"DB CONNECTED"
+    }
