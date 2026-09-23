ad80cb1c1a1441c03aac0a3c9fcd8a01a5530549
Mahesh <maheshh.kumar1508@gmail.com>
2026-09-23 20:11:22 +0530

    models

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from sqlalchemy import create_engne
+from sqlalchemy import column, create_engne, Column, Integer, String, true
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_URL="sqlite:///./test.db"
 
-engine = create_engne(DB_URL, connect_args={"check_same_thred":False})
+engine = create_engine(DB_URL, connect_args={"check_same_thread":False})
 
 sessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+class Todo(Base):
+    __tablename__="todos"
+    id=Column(Integer, primary_key=True, index=True)
+    title=Column(String)
+    completed=Column(String)
+
+Base.metadata.create_all(bind=engine) 
